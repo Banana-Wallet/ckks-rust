34752251dd193ff404e8585df900d7f28e7b72a5
nlok5923 <nlokhande5923@gmail.com>
2024-04-15 11:02:04 +0530

    target feature error while building tfhe

--- ckks-prover/program/src/main.rs
+++ ckks-prover/program/src/main.rs
@@ -10,6 +10,7 @@ use num_bigint::ToBigInt;
 use rlwe::encode;
 use rlwe::encoder;
 use rlwe::*;
+use tfhe_test::run;
 
 use algebra::crt::Crt;
 
@@ -18,6 +19,8 @@ pub fn main() {
     // resulting in output that doesn't match fibonacci sequence.
     // However, the resulting proof will still be valid!
     let n = sp1_zkvm::io::read::<u32>();
+    run();
+
 
     // // let n = 8;
 
@@ -26,9 +29,9 @@ pub fn main() {
     // let x = [0.5, 0.3, 0.78, 0.88];
     // let plainx = encode(&x, 1usize << 30, &encoder);
 
-    let q = 1.to_bigint().unwrap() << 600;
+    // let q = 1.to_bigint().unwrap() << 600;
 
-    let encoder = encoder::CKKSEncoder::new(n as usize * 2);
+    // let encoder = encoder::CKKSEncoder::new(n as usize * 2);
 
     // Create a keypair 100 times
     // for _ in 0..100 {
@@ -56,27 +59,27 @@ pub fn main() {
 
     // let n = 8;
 
-    let q = 1.to_bigint().unwrap() << 600;
+    // let q = 1.to_bigint().unwrap() << 600;
 
-    let key = Rwle::keygen(&q, n as usize, n as usize);
-    let encoder = encoder::CKKSEncoder::new(n as usize * 2);
+    // let key = Rwle::keygen(&q, n as usize, n as usize);
+    // let encoder = encoder::CKKSEncoder::new(n as usize * 2);
 
-    let x = [0.05, 0.1, 1.0, 0.005];
-    let y = [0.1, 0.02, 0.5, 0.3];
+    // let x = [0.05, 0.1, 1.0, 0.005];
+    // let y = [0.1, 0.02, 0.5, 0.3];
 
-    let plainx = encode(&x, 1usize << 30, &encoder);
-    let plainy = encode(&y, 1usize << 30, &encoder);
+    // let plainx = encode(&x, 1usize << 30, &encoder);
+    // let plainy = encode(&y, 1usize << 30, &encoder);
 
-    let cipherx = encrypt(key.public(), &q, &plainx);
-    let ciphery = encrypt(key.public(), &q, &plainy);
+    // let cipherx = encrypt(key.public(), &q, &plainx);
+    // let ciphery = encrypt(key.public(), &q, &plainy);
 
-    let cipherz = &cipherx + &ciphery;
+    // let cipherz = &cipherx + &ciphery;
 
-    let plainz = decrypt(key.private(), cipherz);
+    // let plainz = decrypt(key.private(), cipherz);
 
-    let z = decode(plainz, &encoder);
+    // let z = decode(plainz, &encoder);
 
-    let expected_z: Vec<f64> = x.iter().zip(&y).map(|(a, b)| a + b).collect();
+    // let expected_z: Vec<f64> = x.iter().zip(&y).map(|(a, b)| a + b).collect();
 
     // for (&x, y) in expected_z.iter().zip(z) {
     //     assert_relative_eq!(x, y.re, epsilon = 1e-4)
